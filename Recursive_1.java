f65e877d71c0cb0d39e06d2abdf36b3c904b7c99
AllenChoiwonwoo <dkssudnjsdn@naver.com>
2022-09-15 20:00:30 +0900

    EDIT. 재귀 연습 1-2

--- Recursive_1.java
+++ Recursive_1.java
@@ -18,10 +18,35 @@
  * <p> 클래스 설명 </p>
  * <p> {@link } and {@link }관련 클래스 </p>
  *
- * @see
- * @version 1.0
  * @author allen
+ * @version 1.0
+ * @see
  * @since 지원하는 자바버전 (ex : 5+ 5이상)
  */
 public class Recursive_1 {
+
+	public int factorial(int i) {
+
+		if (i > 0) {
+			return i * factorial(i - 1);
+		} else {
+			return 1;
+		}
+	}
+
+	public int gcd(int x, int y) {
+		System.out.println("x = " + x + " y = " + y);
+		if (y == 0) {
+			return x;
+		} else {
+			return gcd(y, x % y);
+		}
+	}
+
+	public static void main(String[] args) {
+		Recursive_1 recursive_1 = new Recursive_1();
+//		System.out.println(recursive_1.factorial(4));
+
+		System.out.println(recursive_1.gcd(8, 6));
+	}
 }
