9b4442f9774f7e3b4f55daf1d9c2c2bd1cacab7c
AllenChoiwonwoo <dkssudnjsdn@naver.com>
2022-09-15 19:36:04 +0900

    EDIT. 재귀 연습 1-1

---
+++ Recursive_1.java
@@ -0,0 +1,27 @@
+/**
+ * create on 2022/09/15.
+ * create by IntelliJ IDEA.
+ *
+ * <p> 클래스 설명 </p>
+ * <p> {@link } and {@link }관련 클래스 </p>
+ *
+ * @version 1.0
+ * @author allen
+ * @see
+ * @since 지원하는 자바버전 (ex : 5+ 5이상)
+ */
+
+/**
+ * create on 2022/09/15.
+ * create by IntelliJ IDEA.
+ *
+ * <p> 클래스 설명 </p>
+ * <p> {@link } and {@link }관련 클래스 </p>
+ *
+ * @see
+ * @version 1.0
+ * @author allen
+ * @since 지원하는 자바버전 (ex : 5+ 5이상)
+ */
+public class Recursive_1 {
+}
