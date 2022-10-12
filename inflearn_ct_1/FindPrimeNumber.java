e6eaafbea22b28fc3ea841f15d88a7ae43ac0464
AllenChoiwonwoo <dkssudnjsdn@naver.com>
2022-10-12 22:31:53 +0900

    EDIT. 인프런 자바 알고리즘 문제풀이 입문 . 배열 . 5번 -에라토스테네스의 체 - 풀이중

--- inflearn_ct_1/FindPrimeNumber.java
+++ inflearn_ct_1/FindPrimeNumber.java
@@ -20,10 +20,46 @@ package inflearn_ct_1;
  * <p> 클래스 설명 </p>
  * <p> {@link } and {@link }관련 클래스 </p>
  *
- * @see
- * @version 1.0
  * @author allen
+ * @version 1.0
+ * @see
  * @since 지원하는 자바버전 (ex : 5+ 5이상)
  */
 public class FindPrimeNumber {
+
+
+	public static void main(String[] args) {
+		FindPrimeNumber fpn = new FindPrimeNumber();
+		System.out.println(fpn.solution(2));
+	}
+
+	private boolean solution(int val) {
+		int[] primeArr = new int[val];
+
+		// for문에서
+		/*
+		검증하고자 하는 수 n
+		검증
+			m = sqrt n
+			m 길이의 배열 선언
+			2~m 까지 반복하면서
+				n % idx == 0 break
+		 */
+
+		int valSqrt = (int) Math.ceil(Math.sqrt(val));
+		for (int j = 2; j <= valSqrt; j++) {
+			System.out.println(j <= valSqrt);
+			if (val % j == 0) {
+				return false;
+			}
+		}
+		return true;
+
+//		StringBuilder sb = new StringBuilder();
+//		for (int a : primeArr) {
+//			sb.append(a).append(',');
+//		}
+//		System.out.println("sb.toString() = " + sb.toString());
+//		return 0;
+	}
 }
