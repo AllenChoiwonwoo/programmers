944c0628e3de06a91ce5f39375a075ab11a3152d
AllenChoiwonwoo <dkssudnjsdn@naver.com>
2023-01-31 21:59:24 +0900

    ADD . 백준11659 진행중

---
+++ bakjoon/bj11659.java
@@ -0,0 +1,43 @@
+/**
+ * create on 2023/01/31.
+ * create by IntelliJ IDEA.
+ *
+ * <p> 클래스 설명 </p>
+ * <p> {@link } and {@link }관련 클래스 </p>
+ *
+ * @version 1.0
+ * @author allen
+ * @see
+ * @since 지원하는 자바버전 (ex : 5+ 5이상)
+ */
+
+package bakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * create on 2023/01/31.
+ * create by IntelliJ IDEA.
+ *
+ * <p> 클래스 설명 </p>
+ * <p> {@link } and {@link }관련 클래스 </p>
+ *
+ * @see
+ * @version 1.0
+ * @author allen
+ * @since 지원하는 자바버전 (ex : 5+ 5이상)
+ */
+public class bj11659 {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = "";
+		try {
+			str = br.readLine();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		System.out.println("str = " + str);
+	}
+}
