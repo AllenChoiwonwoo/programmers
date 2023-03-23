8831fed92fb9b23369abff365c3f56b30c03b1ec
AllenChoiwonwoo <dkssudnjsdn@naver.com>
2023-03-23 22:20:33 +0900

    ADD . 백준 12891 정답보고 확인 1. 새로들어가는 char, 빠지는 char 만 계산하도록 2. checkSecret 을 지속적으로 업데이트 해주는것 3. 오타 .. -- 로 써야할걸 ++ 로씀

--- bakjoon/bj12891.java
+++ bakjoon/bj12891.java
@@ -18,80 +18,103 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/**
- * create on 2023/03/08.
- * create by IntelliJ IDEA.
- *
- * <p> 클래스 설명 </p>
- * <p> {@link } and {@link }관련 클래스 </p>
- *
- * @author allen
- * @version 1.0
- * @see
- * @since 지원하는 자바버전 (ex : 5+ 5이상)
- */
 public class bj12891 {
+	static int checkArr[];
+	static int myArr[];
+	static int checkSecret;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
-		int dnaLength = Integer.parseInt(stringTokenizer.nextToken()); // DNA 문자열의 길이
-		int dnaPartLength = Integer.parseInt(stringTokenizer.nextToken()); // DNA 부분 문자열의 길이
-
-		stringTokenizer = new StringTokenizer(br.readLine());
-		String dna = stringTokenizer.nextToken(); // DNA 문자열
-//		String dna = "GATA";
-		char[] danCharArr = dna.toCharArray();
-
-//		char[] dnaCharArr = new char[]{ 'A', 'C', 'G', 'T'};
-//									     2    0    1    1
-		int[] dnaCntStandardArr = new int[4];
-		int[] dnaCnt = new int[4];
-
-		stringTokenizer = new StringTokenizer(br.readLine());
-		for (int i = 0; i < dnaCntStandardArr.length; i++) {
-			dnaCntStandardArr[i] = Integer.parseInt(stringTokenizer.nextToken());
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		int S = Integer.parseInt(st.nextToken()); // DNA 문자열의 길이
+		int P = Integer.parseInt(st.nextToken()); // DNA 부분 문자열의 길이
+		int Result = 0;
+		char A[] = new char[S];
+		checkArr = new int[4];
+		myArr = new int[4];
+		checkSecret = 0;
+		A = bf.readLine().toCharArray();
+		st = new StringTokenizer(bf.readLine());
+		for (int i = 0; i < 4; i++) {
+			checkArr[i] = Integer.parseInt(st.nextToken());
+			if (checkArr[i] == 0) {
+				checkSecret++;
+			}
 		}
-
-
-		int result = 0;
-		int start = 0;
-		int end = dnaPartLength;
-
-		while (end <= dnaLength) {
-			boolean isValied = true;
-//			String temp = dna.substring(start, end); 1 2 3 4
-			for (int i = start; i < end; i++) {
-				switch (danCharArr[i]) {
-					case 'A':
-						dnaCnt[0]++;
-						break;
-					case 'C':
-						dnaCnt[1]++;
-						break;
-					case 'G':
-						dnaCnt[2]++;
-						break;
-					case 'T':
-						dnaCnt[3]++;
-						break;
-				}
-			}
-
-			for (int i = 0; i < 4; i++) {
-				if (dnaCnt[i] < dnaCntStandardArr[i]) {
-					isValied = false;
-				}
-			}
-			if (isValied) {
-				result++;
-			}
-			start++;
-			end++;
-			dnaCnt = new int[4];
+		for (int i = 0; i < P; i++) {
+			add(A[i]);
 		}
+		if (checkSecret == 4) {
+			Result++;
+		}
+//		System.out.println("result = " + result);
 
-		System.out.println(result);
+		for (int i = P; i < S; i++) {
+			int j = i - P;
+			add(A[i]);
+			remove(A[j]);
+			if (checkSecret == 4) {
+				Result++;
+			}
+		}
+		System.out.println(Result);
+		bf.close();
+	}
 
+	public static void add(char c){
+		switch (c) {
+			case 'A':
+				myArr[0]++;
+				if (checkArr[0] == myArr[0]) {
+					checkSecret++;
+				}
+				break;
+			case 'C':
+				myArr[1]++;
+				if (checkArr[1] == myArr[1]) {
+					checkSecret++;
+				}
+				break;
+			case 'G':
+				myArr[2]++;
+				if (checkArr[2] == myArr[2]) {
+					checkSecret++;
+				}
+				break;
+			case 'T':
+				myArr[3]++;
+				if (checkArr[3] == myArr[3]) {
+					checkSecret++;
+				}
+				break;
+		}
+	}
+	public static void remove(char c){
+		switch (c) {
+			case 'A':
+				if (checkArr[0] == myArr[0]) {
+					checkSecret--;
+				}
+				myArr[0]--;
+				break;
+			case 'C':
+				if (checkArr[1] == myArr[1]) {
+					checkSecret--;
+				}
+				myArr[1]--;
+				break;
+			case 'G':
+				if (checkArr[2] == myArr[2]) {
+					checkSecret--;
+				}
+				myArr[2]--;
+				break;
+			case 'T':
+				if (checkArr[3] == myArr[3]) {
+					checkSecret--;
+				}
+				myArr[3]--;
+				break;
+		}
 	}
 }
